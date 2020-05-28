b8dff5a4f8768e0e77659236236ae0b2844a25b3
aditditto <aditditto@gmail.com>
2020-05-28 13:00:03 +0700

    Add change size button

--- js/main.js
+++ js/main.js
@@ -22,41 +22,52 @@ function getRandomColor() {
     return color
 }
 
+function rainbowize(e) {
+    this.className = 'boxes'
+    this.style['background-color'] = getRandomColor()
+}
+
 function colorIn(color) {
     const domBoxes = document.getElementsByClassName('boxes')
     if (color === 'rainbow') {
         Array.from(domBoxes).forEach((box) => {
-            box.addEventListener('mouseover', function(e) {
-                box.className = 'boxes'
-                box.style['background-color'] = getRandomColor()
-        })})
+            box.addEventListener('mouseover', rainbowize)})
     }
     else if (color === 'erase') {
         Array.from(domBoxes).forEach((box) => {
+            box.removeEventListener('mouseover', rainbowize)
             box.addEventListener('mouseover', function(e) {
                 box.className = 'boxes'
+                box.style['background-color'] = 'white'
         })})
     }
     else {
         Array.from(domBoxes).forEach((box) => {
+            box.removeEventListener('mouseover', rainbowize)
             box.addEventListener('mouseover', function(e) {
                 box.classList.add('black')
+                box.style['background-color'] = 'black'
         })})
     }
 }
 
 const container = document.getElementById('container');
 
-makeBlanks(16)
-colorIn('black')
+makeBlanks(16);
+colorIn('black');
 
-const rainbowButton = document.getElementById('rainbow')
-rainbowButton.onclick = function() {colorIn('rainbow')}
+const rainbowButton = document.getElementById('rainbow');
+rainbowButton.onclick = function() {colorIn('rainbow')};
 
-const blackButton = document.getElementById('black')
-blackButton.onclick = function() {colorIn('black')}
+const blackButton = document.getElementById('black');
+blackButton.onclick = function() {colorIn('black')};
 
-const eraseButton = document.getElementById('erase')
-eraseButton.onclick = function() {colorIn('erase')}
+const eraseButton = document.getElementById('erase');
+eraseButton.onclick = function() {colorIn('erase')};
 
-
+const newGridButton = document.getElementById('newgrid');
+newGridButton.onclick = function() {
+    container.textContent = ''
+    makeBlanks(prompt('Insert new grid width (default is 16)'));
+    colorIn('black');
+};
